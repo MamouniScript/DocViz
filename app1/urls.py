4e5bd0c207be0c41e83bdc508ab2aaf7c46ef602
Mamouni <84844619+MamouniScript@users.noreply.github.com>
2023-12-29 17:43:40 +0100

    Delete app1 directory

--- app1/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import upload_file, uploaded, display_visualization
-
-urlpatterns = [
-    path('upload/', upload_file, name='upload_file'),
-    path('uploaded/', uploaded, name='uploaded'),
-    path('visualization/', display_visualization, name='display_visualization'),
-]
